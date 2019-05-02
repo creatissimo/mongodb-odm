a8cacb698bdbddc31a6af226914d2961fe46aad9
Serzh Yevtushenko <s.yevtushenko@n-ix.com>
2019-05-02 12:21:43 +0300

    Fork for copy improvement from https://github.com/lhpalacio/mongodb-odm

--- lib/Doctrine/ODM/MongoDB/DocumentRepository.php
+++ lib/Doctrine/ODM/MongoDB/DocumentRepository.php
@@ -26,7 +26,6 @@ use Doctrine\Common\Collections\Selectable;
 use Doctrine\Common\Inflector\Inflector;
 use Doctrine\Common\Persistence\ObjectRepository;
 use Doctrine\ODM\MongoDB\Mapping\ClassMetadata;
-use Doctrine\ODM\MongoDB\Query\QueryExpressionVisitor;
 
 /**
  * A DocumentRepository serves as a repository for documents with generic as well as
@@ -63,16 +62,16 @@ class DocumentRepository implements ObjectRepository, Selectable
      * Initializes this instance with the specified document manager, unit of work and
      * class metadata.
      *
-     * @param DocumentManager $dm The DocumentManager to use.
-     * @param UnitOfWork $uow The UnitOfWork to use.
-     * @param ClassMetadata $classMetadata The class metadata.
+     * @param DocumentManager $dm            The DocumentManager to use.
+     * @param UnitOfWork      $uow           The UnitOfWork to use.
+     * @param ClassMetadata   $classMetadata The class metadata.
      */
     public function __construct(DocumentManager $dm, UnitOfWork $uow, ClassMetadata $classMetadata)
     {
         $this->documentName = $classMetadata->name;
-        $this->dm = $dm;
-        $this->uow = $uow;
-        $this->class = $classMetadata;
+        $this->dm           = $dm;
+        $this->uow          = $uow;
+        $this->class        = $classMetadata;
     }
 
     /**
@@ -107,12 +106,13 @@ class DocumentRepository implements ObjectRepository, Selectable
      * Finds a document matching the specified identifier. Optionally a lock mode and
      * expected version may be specified.
      *
-     * @param mixed $id Identifier.
-     * @param int $lockMode Optional. Lock mode; one of the LockMode constants.
-     * @param int $lockVersion Optional. Expected version.
-     * @throws Mapping\MappingException
-     * @throws LockException
+     * @param mixed $id          Identifier.
+     * @param int   $lockMode    Optional. Lock mode; one of the LockMode constants.
+     * @param int   $lockVersion Optional. Expected version.
+     *
      * @return object|null The document, if found, otherwise null.
+     * @throws LockException
+     * @throws Mapping\MappingException
      */
     public function find($id, $lockMode = LockMode::NONE, $lockVersion = null)
     {
@@ -126,8 +126,8 @@ class DocumentRepository implements ObjectRepository, Selectable
         if (is_array($id)) {
             list($identifierFieldName) = $this->class->getIdentifierFieldNames();
 
-            if (isset($id[$identifierFieldName])) {
-                $id = $id[$identifierFieldName];
+            if (isset($id[ $identifierFieldName ])) {
+                $id = $id[ $identifierFieldName ];
             }
         }
 
@@ -140,7 +140,7 @@ class DocumentRepository implements ObjectRepository, Selectable
             return $document; // Hit!
         }
 
-        $criteria = array('_id' => $id);
+        $criteria = ['_id' => $id];
 
         if ($lockMode === LockMode::NONE) {
             return $this->getDocumentPersister()->load($criteria);
@@ -157,7 +157,7 @@ class DocumentRepository implements ObjectRepository, Selectable
             return $document;
         }
 
-        return $this->getDocumentPersister()->load($criteria, null, array(), $lockMode);
+        return $this->getDocumentPersister()->load($criteria, null, [], $lockMode);
     }
 
     /**
@@ -167,7 +167,7 @@ class DocumentRepository implements ObjectRepository, Selectable
      */
     public function findAll()
     {
-        return $this->findBy(array());
+        return $this->findBy([]);
     }
 
     /**
@@ -189,6 +189,7 @@ class DocumentRepository implements ObjectRepository, Selectable
      * Finds a single document by a set of criteria.
      *
      * @param array $criteria
+     *
      * @return object
      */
     public function findOneBy(array $criteria)
@@ -200,13 +201,14 @@ class DocumentRepository implements ObjectRepository, Selectable
      * Adds support for magic finders.
      *
      * @param string $method
-     * @param array $arguments
-     * @throws MongoDBException
+     * @param array  $arguments
+     *
+     * @return array|object The found document/documents.
+     *
      * @throws \BadMethodCallException If the method called is an invalid find* method
      *                                 or no find* method at all and therefore an invalid
      *                                 method call.
-     * @return array|object The found document/documents.
-     *
+     * @throws MongoDBException
      * @deprecated method was deprecated in 1.2 and will be removed in 2.0
      */
     public function __call($method, $arguments)
@@ -216,10 +218,10 @@ class DocumentRepository implements ObjectRepository, Selectable
             E_USER_DEPRECATED
         );
         if (strpos($method, 'findBy') === 0) {
-            $by = substr($method, 6, strlen($method));
+            $by     = substr($method, 6, strlen($method));
             $method = 'findBy';
         } elseif (strpos($method, 'findOneBy') === 0) {
-            $by = substr($method, 9, strlen($method));
+            $by     = substr($method, 9, strlen($method));
             $method = 'findOneBy';
         } else {
             throw new \BadMethodCallException(
@@ -234,7 +236,7 @@ class DocumentRepository implements ObjectRepository, Selectable
         $fieldName = Inflector::camelize($by);
 
         if ($this->class->hasField($fieldName)) {
-            return $this->$method(array($fieldName => $arguments[0]));
+            return $this->$method([$fieldName => $arguments[0]]);
         } else {
             throw MongoDBException::invalidFindByCall($this->documentName, $fieldName, $method . $by);
         }
@@ -276,34 +278,16 @@ class DocumentRepository implements ObjectRepository, Selectable
      * Selects all elements from a selectable that match the expression and
      * returns a new collection containing these elements.
      *
-     * @see Selectable::matching()
      * @param Criteria $criteria
+     *
      * @return Collection
+     * @see Selectable::matching()
      */
     public function matching(Criteria $criteria)
     {
-        $visitor = new QueryExpressionVisitor($this->createQueryBuilder());
         $queryBuilder = $this->createQueryBuilder();
 
-        if ($criteria->getWhereExpression() !== null) {
-            $expr = $visitor->dispatch($criteria->getWhereExpression());
-            $queryBuilder->setQueryArray($expr->getQuery());
-        }
-
-        if ($criteria->getMaxResults() !== null) {
-            $queryBuilder->limit($criteria->getMaxResults());
-        }
-
-        if ($criteria->getFirstResult() !== null) {
-            $queryBuilder->skip($criteria->getFirstResult());
-        }
-
-        if ($criteria->getOrderings() !== null) {
-            $queryBuilder->sort($criteria->getOrderings());
-        }
-
-        // @TODO: wrap around a specialized Collection for efficient count on large collections
-        return new ArrayCollection($queryBuilder->getQuery()->execute()->toArray(false));
+        return new LazyCriteriaCollection($queryBuilder, $criteria);
     }
 
     protected function getDocumentPersister()
